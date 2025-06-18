3fb70fc9a9416185ef2e87439fee4266c27a4932
user <kleba37@yandex.ru>
2025-06-18 22:16:10 +0300

    [#] feat: add seader room and factory room

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -2,22 +2,25 @@
 
 namespace Database\Factories;
 
+use App\Models\Room;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Room>
+ * @extends Factory<Room>
  */
 class RoomFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+		$reservedDay = Faker\Factory::create('ru_RU')->numberBetween(0, 14);
+		$currentDate = new \DateTimeImmutable();
+
         return [
-            //
+            'number' => Faker\Factory::create('ru_RU')->numberBetween(0, 1000),
+	        'reserved_at' => $currentDate,
+	        'reserved_to' => $currentDate->modify('+' . $reservedDay . 'days')->modify('00:00:00'),
+	        'status' => $this->faker->boolean(),
         ];
     }
 }
--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Room;
 use Illuminate\Database\Seeder;
 
 class RoomSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+		Room::factory()->count(10)->create();
     }
 }
